fe61200ef0bcad3a526e5cb8b6e532dc25e9d7d8
Miguel Angel <Miguel Angel@DESKTOP-R9EUIBP>
2022-09-30 20:00:44 -0500

    Agregar archivos de proyecto.

---
+++ Dev.Api/Dev.Enum/Response/StatusCodeEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Enum.Response
+{
+    /// <summary>
+    /// Enum general para las respuestas a los 
+    /// Endpoints del API
+    /// </summary>
+    public enum StatusCodeEnum
+    {
+        Ok = 200,
+        Found = 302,
+        Bad_Request = 400,
+        Unauthorized = 401,
+        Not_Found = 404
+    }
+}
