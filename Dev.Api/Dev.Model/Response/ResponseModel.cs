e72d454262001beae7df63c2cbd5575fbcced7d9
Mig-Angel <miguelangel.leon@utxicotepec.edu.mx>
2022-09-30 21:59:02 -0500

    Enum, Model y Util

---
+++ Dev.Api/Dev.Model/Response/ResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using Dev.Enum.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dev.Model.Response
+{
+    /// <summary>
+    /// Clase general para las respuestas a los 
+    /// Endpoints del API
+    /// </summary>
+    /// <typeparam name="T">Clase del objeto se almacenara</typeparam>
+    public class ResponseModel<T>
+    {
+        public StatusCodeEnum Code { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
+
+    }
+}
